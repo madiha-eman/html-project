d6f70798e9e9bc37f68c8df2fad7306bf35a021a
madiha-eman <madihaema1@gmail.com>
2021-08-16 20:29:05 +0500

    indexclone done

---
+++ Mr.Jhon's project/Ehtesham's Project/sidebar-09/UpperSectionPages/new for Qazi/js/app.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('button');
+
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const para = button.nextElementSibling;
+        const icon = button.children[1];
+
+        para.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
